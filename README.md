e68583bb705705ab2bd553b05e357131c1909b06
Chris Falck <chris.falck@tickboxconsulting.com>
2017-04-22 00:20:57 +0100

    Provision Puppet and Python 3.6.1
    Added Vagrantfile to provision the following:
    
    O/S: CentOS 7
    Puppet
    Python 3.6.1
    
    Scripts
    =======
    
    centos_7.x.sh - Provision Puppet
    python_3.6.sh - Provision Python 3.6.1, PIP and adds Development
    Compilers and related tools; Libraries needed during compilation to
    enable all features of Python: zlib-devel bzip2-devel openssl-devel
    ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel
    libpcap-devel xz-devel expat-devel
    Python 3.6.1 is installed into /usr/local/bin as python3.6

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # base-centos7x-puppet
 Base development vagrant installation which installs CentOS 7x together with Puppet for provisioning software
+
+Getting setup
+-------------
+
+The only moving piece you need installed on your system is Vagrant: https://www.vagrantup.com/
+
+Puppet does NOT need to be on your workstation since we’re only going to be using it on the VMs that Vagrant spins up.
+
+Vagrantfile
+-----------
+
+The Vagrant file is where the creation of the repeatable development environment workflow is controlled. Sets up the network and gets Puppet installed via the Shell Provisioner where the shell script is executed provided here https://github.com/hashicorp/puppet-bootstrap/
+
+Puppet
+------
+
+We will pass Puppet code via a shared folder.
---
+++ src/python_3.6.sh
@@ -0,0 +1,31 @@
+!/usr/bin/env bash
+# This bootstraps Python on CentOS 7.x
+
+set -e
+
+#REPO_URL="http://python.org/ftp/python/3.6.0/Python-3.6.0.tar.xz"
+REPO_URL="https://www.python.org/ftp/python/3.6.1/Python-3.6.1.tar.xz"
+GET_PIP_URL="https://bootstrap.pypa.io/get-pip.py"
+
+# Compilers and related tools:
+yum groupinstall -y "development tools"
+
+# Libraries needed during compilation to enable all features of Python:
+yum install -y zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel expat-devel
+
+# If you are on a clean "minimal" install of CentOS you also need the wget tool:
+yum install -y wget
+
+# Python 3.6.1:
+wget "${REPO_URL}"
+tar xf Python-3.6.1.tar.xz
+cd Python-3.6.1
+./configure --prefix=/usr/local --enable-shared LDFLAGS="-Wl,-rpath /usr/local/lib"
+make && make altinstall
+
+# First get the script:
+wget "${GET_PIP_URL}"
+
+# Then execute it using Python 3.6:
+/usr/local/bin/python3.6 get-pip.py
+
